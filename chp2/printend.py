5f12ebf4a00bcd861bd76e61d3ded5d0cb591839
Frank Larios <frankandrewlarios@gmail.com>
2017-11-07 19:32:02 -0800

    adds some example code from book

---
+++ chp2/printend.py
@@ -0,0 +1,7 @@
+# printend.py
+# Program that illustrates the use of the "end" keyword as it applies to
+# Python's built-in print function, to combine multiple print statements
+# that produce a single line of output.
+
+print("The answer is", end=" ")
+print(3 + 4)
